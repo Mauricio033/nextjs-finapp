b74d4a12c45f9852d6c1113bd8a3e4d0eb6250ce
Mauricio033 <mauriciogago6@gmail.com>
2025-09-06 23:53:08 +0700

    Sidenav added

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from 'react'
 import { redirect } from 'next/navigation'
 import { createSupabaseServer } from '@/lib/supabase/server'
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/ui/app-sidebar"
 
 export default async function AuthLayout({ children }: { children: ReactNode }) {
   const supabase = await createSupabaseServer()
   const { data: { session } } = await supabase.auth.getSession()
   if (!session) redirect('/login')
   return (
-    <>
-      
-      <div className="flex-1">{children}</div>
-    </>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset className="flex min-h-svh w-full flex-col">
+        <div className="p-6">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,125 @@
+import {
+  Calendar,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+  User2,
+  Link,
+} from "lucide-react";
+
+import Image from "next/image";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "dashboard",
+    icon: Home,
+  },
+  {
+    title: "Transactions",
+    url: "transactions",
+    icon: Inbox,
+  },
+  {
+    title: "Accounts",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Users",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+          <Image
+            src="/FinApp_logo.png"
+            alt="FinApp"
+            width={621}
+            height={212}
+            className="h-10 w-40"
+          />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> Username
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
